b6cd20de195bf1007f8df82ae527ccedbc10687f
Cesar Gonzalez <gcesar13@yahoo.com>
2022-11-02 20:21:31 -0600

    comment route

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
 import sys
+from app.models import User, Post, Comment, Vote
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -55,6 +56,28 @@ def login():
   session.clear()
   session['user_id'] = user.id
   session['loggedIn'] = True
-  
+
   return jsonify(id = user.id)
 
+@bp.route('/comments', methods=['POST'])
+def comment():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new comment
+    newComment = Comment(
+      comment_text = data['comment_text'],
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newComment)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Comment failed'), 500
+  
+  return jsonify(id = newComment.id)
